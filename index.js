cb5d97b8a2e402f1d287c5759df6da621b9225de
Lucas Natoli <lucas.natoli@gmail.com>
2019-11-17 23:39:07 -0300

    added CORS headers

--- index.js
+++ index.js
@@ -20,6 +20,15 @@ app.use(session({
   cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 }
 }))
 
+// Add CORS headers
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 router(app, models)
 
 //drop and resync with { force: true }
